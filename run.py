4328fca12b43855e2710d26b349a4db8d3a9f770
sanchezz <checking99@mail.ru>
2025-10-20 16:00:17 +0600

    bluat

--- run.py
+++ run.py
@@ -2,13 +2,15 @@ from aiogram import Bot, Dispatcher, types
 from aiogram.filters import Command
 from aiogram.enums import ParseMode
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from aiogram.client.default import DefaultBotProperties
 import asyncio
 import os
 from aiohttp import web
 
 # ====== Токен через Environment Variable ======
 TOKEN = os.getenv("BOT_TOKEN")
-bot = Bot(token=TOKEN, parse_mode=ParseMode.HTML)
+#bot = Bot(token=TOKEN, parse_mode=ParseMode.HTML)
+bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
 dp = Dispatcher()
 scheduler = AsyncIOScheduler()
 
